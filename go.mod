7e56b4b322d5bae6cc48311d205c81b9e00d1264
Dirk Tomkel <dirk.tomkel@posteo.de>
2025-11-16 21:41:33 +0100

    replace xml parser

--- go.mod
+++ go.mod
@@ -2,11 +2,4 @@ module github.com/dirk-tomkel/add-scenes
 
 go 1.25.4
 
-require github.com/antchfx/xmlquery v1.5.0
-
-require (
-	github.com/antchfx/xpath v1.3.5 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	golang.org/x/net v0.33.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-)
+require github.com/beevik/etree v1.6.0
--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"encoding/csv"
-	"encoding/xml"
 	"flag"
 
 	"log"
 	"os"
 
-	"github.com/antchfx/xmlquery"
+	"github.com/beevik/etree"
 )
 
 func main() {
@@ -17,15 +16,9 @@ func main() {
 	flag.Parse()
 
 	// -- lese kdenliveFile ein --
-	kf, err := os.Open(*kdenliveFile)
-	if err != nil {
+	doc := etree.NewDocument()
+	if err := doc.ReadFromFile(*kdenliveFile); err != nil {
 		log.Fatal("Fehler: Kann datei nicht einlesen:", err)
-	}
-
-	// -- XML parsen --
-	doc, err := xmlquery.Parse(kf)
-	if err != nil {
-		log.Fatal("Fehler: Kann XML nicht parsen:", err)
 	}
 
 	// -- lese CSV datei ein --
@@ -46,65 +39,49 @@ func main() {
 
 	// Finde Playlisten mit Einträgen
 	var kdenliveId string
-	playlists := xmlquery.Find(doc, "//playlist[entry and @id != 'main_bin']")
+	playlists := doc.FindElements("//playlist[entry]")
 	for _, playlist := range playlists {
 		// lösche bestehende Einträge
-		playlistId := playlist.SelectAttr("id")
-		log.Printf("Lösche Einträge in %s.\n", playlistId)
-		producerChain := xmlquery.FindOne(playlist, "//entry[@producer]").SelectAttr("producer")
-		playlist.FirstChild = nil
-		playlist.LastChild = nil
+		playlistId := playlist.SelectAttrValue("id", "keine Playlist")
+		if playlistId != "main_bin" {
+			log.Printf("Lösche Einträge in %s.\n", playlistId)
+			producerChain := playlist.FindElement("//entry[@producer]").SelectAttrValue("producer", "keine Ahnung")
+			playlist.Child = nil
 
-		// -- Schreibe Szenen-Einträge aus CSV Datei in Playlists
-		// ignoreire die ersten beiden zeilen
-		var sceneCount int
-		for _, row := range csvData[2:] {
-			start := row[2]
-			ende := row[5]
-			addEntry(playlist, start, ende, producerChain, kdenliveId)
-			sceneCount++
+			// -- Schreibe Szenen-Einträge aus CSV Datei in Playlists
+			// ignoreire die ersten beiden zeilen
+			var sceneCount int
+			for _, row := range csvData[2:] {
+				start := row[2]
+				ende := row[5]
+				addEntry(playlist, start, ende, producerChain, kdenliveId)
+				sceneCount++
+			}
+
+			log.Printf("Füge %d Szenen zu producer=%s hinzu.\n", sceneCount, producerChain)
 		}
-
-		log.Printf("Füge %d Szenen zu producer=%s hinzu.\n", sceneCount, producerChain)
 	}
 
 	// -- Erstelle die Kdenlive AUsgabe-Datei --
 	finalOutputFilename := "scenes-" + *kdenliveFile
-	outFile, err := os.Create(finalOutputFilename)
-	if err != nil {
-		log.Fatal("Fehler: kann Szenen-Datei nicht erstellen.", err)
-	}
-	defer outFile.Close()
-
-	err = doc.WriteWithOptions(outFile)
+	doc.Indent(2)
+	err = doc.WriteToFile(finalOutputFilename)
 	if err != nil {
 		log.Fatal("Fehler: kann XML nicht schreiben.", err)
 	}
 }
 
-func addEntry(playlist *xmlquery.Node, in, out, producer, id string) {
-	entry := &xmlquery.Node{
-		Type: xmlquery.ElementNode,
-		Data: "entry",
-		Attr: []xmlquery.Attr{
-			{Name: xml.Name{Local: "in"}, Value: in},
-			{Name: xml.Name{Local: "out"}, Value: out},
-			{Name: xml.Name{Local: "producer"}, Value: producer},
-		},
-	}
+func addEntry(playlist *etree.Element, in, out, producer, id string) {
+	// 1. <entry in="..." out="..." producer="...">
+	entry := playlist.CreateElement("entry")
+	entry.CreateAttr("in", in)
+	entry.CreateAttr("out", out)
+	entry.CreateAttr("producer", producer)
 
-	property := &xmlquery.Node{
-		Type: xmlquery.ElementNode,
-		Data: "property",
-		Attr: []xmlquery.Attr{
-			{Name: xml.Name{Local: "name"}, Value: "kdenlive:id"},
-		},
-		FirstChild: &xmlquery.Node{
-			Type: xmlquery.TextNode,
-			Data: id,
-		},
-	}
+	// 2. <property name="kdenlive:id"><id-value></property>
+	property := entry.CreateElement("property")
+	property.CreateAttr("name", "kdenlive:id")
 
-	xmlquery.AddChild(entry, property)
-	xmlquery.AddChild(playlist, entry)
+	// Textinhalt der property
+	property.CreateText(id)
 }
